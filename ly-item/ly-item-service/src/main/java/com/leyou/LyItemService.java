28e41886286ec3897adffb33b029d0756589a926
cwx2810 <cwx2810@163.com>
2018-08-25 00:13:45 +0800

    搭建商品微服务及项目启动测试

--- ly-item/ly-item-service/src/main/java/com/leyou/LyItemService.java
+++ ly-item/ly-item-service/src/main/java/com/leyou/LyItemService.java
@@ -1,13 +1,13 @@
+package com.leyou;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-@EnableZuulProxy
-public class LyApiGateway {
+public class LyItemService {
     public static void main(String[] args) {
-        SpringApplication.run(LyApiGateway.class, args);
+        SpringApplication.run(LyItemService.class, args);
     }
 }
